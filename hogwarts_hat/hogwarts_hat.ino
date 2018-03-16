5ce8d02b8f9365b0764548e3cb4e748e910c86c0
Unknown <13784757+Snackya@users.noreply.github.com>
2018-03-16 16:22:16 +0100

    add gyro; change folder structure

---
+++ hogwarts_hat/hogwarts_hat.ino
@@ -0,0 +1,56 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_L3GD20_U.h>
+#include <SoftwareSerial.h>
+#include <DFRobotDFPlayerMini.h>
+
+DFRobotDFPlayerMini myDFPlayer;
+Adafruit_L3GD20_Unified gyro = Adafruit_L3GD20_Unified(20);
+SoftwareSerial mySoftwareSerial(11, 10); // RX, TX
+
+void setup() {
+
+  mySoftwareSerial.begin(9600);
+  Serial.begin(115200);
+  
+  pinMode(7, OUTPUT);
+  //digitalWrite(7, HIGH); keep Arduino on
+
+  if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
+    Serial.println(F("Unable to begin:"));
+    Serial.println(F("1.Please recheck the connection!"));
+    Serial.println(F("2.Please insert the SD card!"));
+    while(true);
+  }
+  Serial.println(F("DFPlayer Mini online."));
+  
+  gyro.enableAutoRange(true);  
+  if(!gyro.begin())
+  {
+    /* There was a problem detecting the L3GD20 ... check your connections */
+    Serial.println("Ooops, no L3GD20 detected ... Check your wiring!");
+    while(1);
+  }
+  
+  myDFPlayer.setTimeOut(500);
+  myDFPlayer.volume(20); //set volume (0-30)
+  //----Set different EQ----
+  myDFPlayer.EQ(DFPLAYER_EQ_NORMAL);
+  //----Set device we use SD as default----
+  myDFPlayer.outputDevice(DFPLAYER_DEVICE_SD);
+  //----Mp3 play----
+  myDFPlayer.next();  //Play next mp3
+}
+
+void loop() {
+  /* Get a new sensor event */ 
+  sensors_event_t event; 
+  gyro.getEvent(&event);
+  
+  Serial.print("X: "); Serial.print(event.gyro.x); Serial.print("  ");
+  Serial.print("Y: "); Serial.print(event.gyro.y); Serial.print("  ");
+  Serial.print("Z: "); Serial.print(event.gyro.z); Serial.print("  ");
+  Serial.println("rad/s ");
+  delay(500);
+  //digitalWrite(7, LOW); Turn off arduino
+}
