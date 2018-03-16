3adabdd08eba38e6870c22dfb2dee0de2b937013
Unknown <13784757+Snackya@users.noreply.github.com>
2018-03-16 17:21:32 +0100

    add threshold

--- hogwarts_hat/hogwarts_hat.ino
+++ hogwarts_hat/hogwarts_hat.ino
@@ -8,28 +8,53 @@ DFRobotDFPlayerMini myDFPlayer;
 Adafruit_L3GD20_Unified gyro = Adafruit_L3GD20_Unified(20);
 SoftwareSerial mySoftwareSerial(11, 10); // RX, TX
 
-void setup() {
+unsigned long previousMillis = 0;
+const long interval = 1000;
+const long scanInterval = 100;
+bool askingForInput = false;
+float inputTreshold = 0.2;
 
+void setup() {
   mySoftwareSerial.begin(9600);
   Serial.begin(115200);
   
   pinMode(7, OUTPUT);
   //digitalWrite(7, HIGH); keep Arduino on
-
-  if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
-    Serial.println(F("Unable to begin:"));
-    Serial.println(F("1.Please recheck the connection!"));
-    Serial.println(F("2.Please insert the SD card!"));
-    while(true);
-  }
-  Serial.println(F("DFPlayer Mini online."));
   
-  gyro.enableAutoRange(true);  
-  if(!gyro.begin())
-  {
-    /* There was a problem detecting the L3GD20 ... check your connections */
-    Serial.println("Ooops, no L3GD20 detected ... Check your wiring!");
-    while(1);
+  setupGyro();
+  setupDFPlayer();
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+
+  /* Get a new sensor event */ 
+  sensors_event_t event; 
+  gyro.getEvent(&event);
+
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    Serial.print("Y: "); Serial.print(event.gyro.y); Serial.print("  ");
+    Serial.print("Z: "); Serial.print(event.gyro.z); Serial.print("  ");
+    Serial.println("rad/s ");
+  }
+  if(isAskingForInput(){
+    float yData = event.gyro.y;
+    float zData = event.gyro.z;
+
+    if(yData > inputTreshold || yData < -inputTreshold || zData > inputThreshold || zData < -inputThreshold){
+      //get average values
+      //return nicken/schuetteln bool
+    }
+  }
+
+  //digitalWrite(7, LOW); Turn off arduino
+  
+}
+
+void setupDFPlayr(){
+  if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
+    while(true);
   }
   
   myDFPlayer.setTimeOut(500);
@@ -42,15 +67,11 @@ void setup() {
   myDFPlayer.next();  //Play next mp3
 }
 
-void loop() {
-  /* Get a new sensor event */ 
-  sensors_event_t event; 
-  gyro.getEvent(&event);
-  
-  Serial.print("X: "); Serial.print(event.gyro.x); Serial.print("  ");
-  Serial.print("Y: "); Serial.print(event.gyro.y); Serial.print("  ");
-  Serial.print("Z: "); Serial.print(event.gyro.z); Serial.print("  ");
-  Serial.println("rad/s ");
-  delay(500);
-  //digitalWrite(7, LOW); Turn off arduino
+void setupGyro(){
+  gyro.enableAutoRange(true);  
+  if(!gyro.begin())
+  {
+    while(1);
+  }
 }
+
