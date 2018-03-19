10a88762d0192698324105e1a9ba8f17e62602fe
Unknown <13784757+Snackya@users.noreply.github.com>
2018-03-19 19:45:28 +0100

    add sound, distance sensor

--- hogwarts_hat/hogwarts_hat.ino
+++ hogwarts_hat/hogwarts_hat.ino
@@ -3,74 +3,65 @@
 #include <Adafruit_L3GD20_U.h>
 #include <SoftwareSerial.h>
 #include <DFRobotDFPlayerMini.h>
+#include <CapacitiveSensor.h>
+
+const static int RXpin = 11;
+const static int TXpin = 10;
 
 DFRobotDFPlayerMini myDFPlayer;
 Adafruit_L3GD20_Unified gyro = Adafruit_L3GD20_Unified(20);
-SoftwareSerial mySoftwareSerial(11, 10); // RX, TX
-
-unsigned long previousMillis = 0;
-const long interval = 1000;
-const long scanInterval = 100;
-bool askingForInput = false;
-float inputTreshold = 0.2;
+SoftwareSerial mySoftwareSerial(RXpin, TXpin);
+CapacitiveSensor cs_8_7 = CapacitiveSensor(8,7);
 
 void setup() {
+  cs_8_7.set_CS_AutocaL_Millis(0xFFFFFFFF);
   mySoftwareSerial.begin(9600);
   Serial.begin(115200);
-  
-  pinMode(7, OUTPUT);
-  //digitalWrite(7, HIGH); keep Arduino on
-  
+
   setupGyro();
   setupDFPlayer();
 }
 
 void loop() {
-  unsigned long currentMillis = millis();
-
-  /* Get a new sensor event */ 
+  /* Get a new sensor event */
+  long start = millis();
+  long total1 =  cs_8_7.capacitiveSensor(30);
   sensors_event_t event; 
   gyro.getEvent(&event);
 
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
-    Serial.print("Y: "); Serial.print(event.gyro.y); Serial.print("  ");
-    Serial.print("Z: "); Serial.print(event.gyro.z); Serial.print("  ");
-    Serial.println("rad/s ");
-  }
-  if(isAskingForInput(){
-    float yData = event.gyro.y;
-    float zData = event.gyro.z;
-
-    if(yData > inputTreshold || yData < -inputTreshold || zData > inputThreshold || zData < -inputThreshold){
-      //get average values
-      //return nicken/schuetteln bool
-    }
-  }
-
-  //digitalWrite(7, LOW); Turn off arduino
+   Serial.print(millis() - start);        // check on performance in milliseconds
+   Serial.print("\t");                    // tab character for debug window spacing
+   Serial.print(total1);                  // print sensor output 1
+   Serial.println();
   
+  Serial.print("X: "); Serial.print(event.gyro.x); Serial.print("  ");
+  Serial.print("Y: "); Serial.print(event.gyro.y); Serial.print("  ");
+  Serial.print("Z: "); Serial.print(event.gyro.z); Serial.print("  ");
+  Serial.println("rad/s ");
+  Serial.println();
+  delay(500);
 }
 
-void setupDFPlayr(){
+void setupDFPlayer(){
   if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
     while(true);
   }
   
   myDFPlayer.setTimeOut(500);
-  myDFPlayer.volume(20); //set volume (0-30)
+  myDFPlayer.volume(5); //set volume (0-30)
   //----Set different EQ----
   myDFPlayer.EQ(DFPLAYER_EQ_NORMAL);
   //----Set device we use SD as default----
   myDFPlayer.outputDevice(DFPLAYER_DEVICE_SD);
   //----Mp3 play----
-  myDFPlayer.next();  //Play next mp3
+  myDFPlayer.play(2);  //Play next mp3
 }
 
 void setupGyro(){
-  gyro.enableAutoRange(true);  
+  gyro.enableAutoRange(true); 
   if(!gyro.begin())
   {
+    Serial.println("Ooops, no L3GD20 detected ... Check your wiring!");
     while(1);
   }
 }
